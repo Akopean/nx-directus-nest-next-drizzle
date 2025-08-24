214b81b0a38dd421e9b8802bb226355a82dbd3be
Agasi <ahasi.akopian@beetroot.se>
2025-08-24 12:45:41 +0300

    test

--- api/src/directus/api/v1/v1.module.ts
+++ api/src/directus/api/v1/v1.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DirectusController } from './controllers/directus.controller';
-import { DirectusService } from './services/directus.service';
 
 @Module({
   controllers: [DirectusController],
-  providers: [DirectusService],
 })
 export class V1Module {}
